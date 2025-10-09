4795d93ace2f05f0f2f9ae99f38920777a933d1e
highlander <bithighlander@gmail.com>
2025-10-09 17:54:16 -0600

    perf: Commit generated API clients for faster Vercel builds
    - Remove packages/unchained-client/src/generated from .gitignore
    - Skip 'generate' step in build:packages (saves ~5 minutes)
    - Commit all 3MB of pre-generated API client code
    
    Impact: Vercel builds now take ~7 minutes instead of ~12 minutes
    
    Note: Regenerate when OpenAPI specs change:
      yarn workspace @shapeshiftoss/unchained-client generate

---
+++ packages/unchained-client/src/generated/optimism/models/index.ts
@@ -0,0 +1,27 @@
+// @ts-nocheck
+/* tslint:disable */
+/* eslint-disable */
+export * from './Account';
+export * from './BadRequestError';
+export * from './BaseInfo';
+export * from './BaseTxHistoryTx';
+export * from './DoRpcRequest200Response';
+export * from './DoRpcRequestRequest';
+export * from './EstimateGasBody';
+export * from './Fees';
+export * from './InternalServerError';
+export * from './InternalTx';
+export * from './OptimismGasEstimate';
+export * from './OptimismGasFees';
+export * from './RPCRequest';
+export * from './RPCResponse';
+export * from './RPCResponseError';
+export * from './RPCResponseId';
+export * from './SendTxBody';
+export * from './TokenBalance';
+export * from './TokenMetadata';
+export * from './TokenMetadataMedia';
+export * from './TokenTransfer';
+export * from './TokenType';
+export * from './Tx';
+export * from './ValidationError';
